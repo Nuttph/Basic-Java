c19cf68ac7931cfce4e7e88fbe440f95479b20c5
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-14 22:29:54 +0700

    Display

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
 class HelloWorld {
     public static void main(String[] arg) {
-        System.out.println("Hello world!!!");
+        System.out.println("printline");
+
+        // Display
+        System.out.print("Welcome Nuttaphon");
+        System.out.println(" Hello Nuttaphon");
+        // Comment
+        System.out.print("New Line");
     }
 
 }
