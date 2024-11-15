d20914cea4183d74c4bf92842fec752de4197c73
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-11-15 11:10:14 +0700

    Create OperatorAndLogic.java

---
+++ OperatorAndLogic.java
@@ -0,0 +1,19 @@
+public class OperatorAndLogic {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+        int b = 2;
+        int c = 3;
+        int d = 10;
+        // Operator
+        System.out.println("a / c :" + (a / d));
+        System.out.println("a % c :" + (a % c)); // 10/3 เศษ 1
+        System.out.println("a % c :" + (a % c));
+
+        System.out.println("a < b :" + (a < b));
+        System.out.println("a > b :" + (a > b));
+        System.out.println("a >= d :" + (a >= d));
+        System.out.println("a == d :" + (a == d));
+        System.out.println("a != d :" + (a != d));
+    }
+}
